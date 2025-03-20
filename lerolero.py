a948c4d6ff37dbefccf9f177fbbd699663866e06
Rafael Carnauskas <carnauskasrafael@mail.com>
2025-03-20 10:33:29 -0300

    Adiciona esqueleto do programa

--- lerolero.py
+++ lerolero.py
@@ -3,3 +3,10 @@ gerador de lero lero
 
 gera frases de efeito sem sentido real
 """
+import random
+
+parte1 = []
+parte2 = []
+parte3 = []
+
+print (random.choice(parte1), random.choice(parte2), random.choice(parte3))
