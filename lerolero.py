4baf9bdf5c930cb8304bd93f95f2163acc67f230
Pedro Porto <pedro.origuela@usp.br>
2025-03-20 10:44:31 -0300

    Cria esqueleto da versão multi linguas

--- lerolero.py
+++ lerolero.py
@@ -5,8 +5,19 @@ gera frases de efeito sem sentido real
 """
 import random
 
+# Cada frase é composta por 3 partes aleatorias,
+# aqui , listas de possibilidades para cada uma das partes
+
 parte1 = []
 parte2 = []
 parte3 = []
 
+lingua = int(input("Digite 1 para português ou 2 para inglês: "))
+
+if lingua == 2:
+    parte1 = []
+    parte2 = []
+    parte3 = []
+
+# combina as partes aleatoriamente
 print (random.choice(parte1), random.choice(parte2), random.choice(parte3))
